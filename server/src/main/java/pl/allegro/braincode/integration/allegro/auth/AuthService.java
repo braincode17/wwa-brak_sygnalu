666dc5afc75d59be523ff8006cd98616942cf786
Marcin Łagowski <marcinlagowski92@gmail.com>
2017-03-24 21:55:22 +0100

    added auth parameters

---
+++ server/src/main/java/pl/allegro/braincode/integration/allegro/auth/AuthService.java
@@ -0,0 +1,14 @@
+package pl.allegro.braincode.integration.allegro.auth;
+
+import org.springframework.stereotype.Service;
+import retrofit2.Call;
+
+@Service
+public class AuthService {
+
+    private String clientID = "a41f5b2a-8e87-4b8b-b6fe-74cc763720d7";
+    private String clientSecret = "bxbb2gFqCP1aM3kNPeptAWQMGz9gosbe9JCO1sqlp0BhY9G4UufpkXgsSFQYE545";
+    private String apiKey = "eyJjbGllbnRJZCI6ImE0MWY1YjJhLThlODctNGI4Yi1iNmZlLTc0Y2M3NjM3MjBkNyJ9.ogVV_a9RUOMa1OWFZOTmgTkdk-U37vTliDCBUQ1YySU=";
+    private String grantType = "client_credentials";
+
+}
