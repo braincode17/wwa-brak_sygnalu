9f6e91f3d39f48081721e6e6477459a42a520c31
Marcin Łagowski <marcinlagowski92@gmail.com>
2017-03-25 03:14:34 +0100

    Added query params

--- server/src/main/java/pl/allegro/braincode/integration/allegro/offers/OffersService.java
+++ server/src/main/java/pl/allegro/braincode/integration/allegro/offers/OffersService.java
@@ -5,6 +5,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+import java.util.Map;
 
 public interface OffersService {
     @Headers({
@@ -13,5 +16,10 @@ public interface OffersService {
             "Accept: application/vnd.allegro.public.v1+json"
     })
     @GET("/offers")
-    public Call<OffersList> offers(@Query("country.code") String countryCode, @Query("phrase") String phrase);
+    public Call<OffersList> offers(
+            @Query("country.code") String countryCode,
+            @Query("phrase") String phrase,
+            @Query("category.id") String categoryId,
+            @QueryMap Map<String, String> map
+    );
 }
