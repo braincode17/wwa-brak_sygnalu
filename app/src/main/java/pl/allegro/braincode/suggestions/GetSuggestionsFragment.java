fd6c71953854c89d12a11dee8d9854ce24c8f066
Paweł Szychiewicz <pawel.szychiewicz@gmail.com>
2017-03-25 03:26:29 +0100

    get suggestions layout

--- app/src/main/java/pl/allegro/braincode/suggestions/GetSuggestionsFragment.java
+++ app/src/main/java/pl/allegro/braincode/suggestions/GetSuggestionsFragment.java
@@ -1,13 +1,31 @@
 package pl.allegro.braincode.suggestions;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
 
 import pl.allegro.braincode.activities.MainActivity;
 import pl.allegro.braincode.R;
@@ -15,6 +33,10 @@ import pl.allegro.braincode.R;
 public class GetSuggestionsFragment extends Fragment {
 
     private MainActivity mainActivity;
+    private LineChart chart;
+    private MaterialSearchView searchView;
+
+    private ArrayList<Entry> chartValues = new ArrayList<Entry>();
 
     public GetSuggestionsFragment() {
     }
@@ -27,6 +49,15 @@ public class GetSuggestionsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
     }
 
     @Override
@@ -38,8 +69,124 @@ public class GetSuggestionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_get_suggestions, container, false);
-        Button finish = (Button)view.findViewById(R.id.search);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        chart = (LineChart) view.findViewById(R.id.chart);
+
+
+        LineData data = getData(36, 100);
+//        mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Bold.ttf");
+//        data.setValueTypeface(mTf);
+
+        // add some transparency to the color with "& 0x90FFFFFF"
+        setupChart(chart, data, Color.rgb(250, 104, 104));
+        searchView = (MaterialSearchView) view.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                setupChart(chart, getData(36, 100), Color.rgb(250, 104, 104));
+                //rest query saving data to chartValues an updating
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+
         return view;
+    }
+
+    private void setupChart(LineChart chart, LineData data, int color) {
+
+        ((LineDataSet) data.getDataSetByIndex(0)).setCircleColorHole(color);
+
+        // no description text
+        chart.getDescription().setEnabled(false);
+
+        // mChart.setDrawHorizontalGrid(false);
+        //
+        // enable / disable grid background
+        chart.setDrawGridBackground(false);
+//        chart.getRenderer().getGridPaint().setGridColor(Color.WHITE & 0x70FFFFFF);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+        chart.setBackgroundColor(color);
+
+        // set custom chart offsets (automatic offset calculation is hereby disabled)
+        chart.setViewPortOffsets(10, 0, 10, 0);
+
+        // add data
+        chart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+        l.setEnabled(false);
+
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisLeft().setSpaceTop(40);
+        chart.getAxisLeft().setSpaceBottom(40);
+        chart.getAxisRight().setEnabled(false);
+
+        chart.getXAxis().setEnabled(false);
+
+        // animate calls invalidate()...
+        chart.animateX(500);
+    }
+
+    private LineData getData(int count, float range) {
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 3*i;
+            yVals.add(new Entry(i, val));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        set1.setLineWidth(1.75f);
+        set1.setCircleRadius(5f);
+        set1.setCircleHoleRadius(2.5f);
+        set1.setColor(Color.WHITE);
+        set1.setCircleColor(Color.WHITE);
+        set1.setHighLightColor(Color.WHITE);
+        set1.setDrawValues(false);
+
+        // create a data object with the datasets
+        LineData data = new LineData(set1);
+
+        return data;
     }
 
     private void showMessageIfViewExists(String message, View view) {
