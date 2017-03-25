da3c6a9c2ccac755c23bed0f9731d523699ef570
Marcin Łagowski <marcinlagowski92@gmail.com>
2017-03-25 09:17:39 +0100

    improved view

--- app/src/main/java/pl/allegro/braincode/fragments/GetSuggestionsFragment.java
+++ app/src/main/java/pl/allegro/braincode/fragments/GetSuggestionsFragment.java
@@ -114,8 +114,8 @@ public class GetSuggestionsFragment extends BaseFragment {
         chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
             public void onValueSelected(Entry e, Highlight h) {
-                daysView.setText(String.valueOf(e.getX()));
-                priceView.setText(String.valueOf(e.getY()));
+                daysView.setText(prettyPrintDuration((int) e.getX()));
+                priceView.setText(String.valueOf(e.getY()) + " zł");
             }
 
             @Override
@@ -151,5 +151,29 @@ public class GetSuggestionsFragment extends BaseFragment {
 
     public void showError(String error) {
         Snackbar.make(getView(), error, Snackbar.LENGTH_SHORT);
+    }
+
+    static String prettyPrintDuration(int days) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int restDays = days;
+        if (restDays > 365) {
+            int years = restDays / 365;
+            restDays = restDays % 365;
+            stringBuffer.append(years).append(" lat, ");
+        }
+        if (restDays > 30) {
+            int months = restDays / 30;
+            restDays = restDays % 30;
+            stringBuffer.append(months).append(" miesięcy, ");
+        }
+        if (restDays > 7) {
+            int weeks = restDays / 7;
+            restDays = restDays % 7;
+            stringBuffer.append(weeks).append(" tygodni, ");
+        }
+        if (restDays > 0) {
+            stringBuffer.append(restDays).append(" dni");
+        }
+        return stringBuffer.toString();
     }
 }
