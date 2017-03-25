1171226f6e4af97f510eb480380776ec59af621a
Karol <karol.szpakowski@gmail.com>
2017-03-25 01:22:02 +0100

    add java time to jackson mapper

--- server/src/main/java/pl/allegro/braincode/api/config/RestApiConfig.java
+++ server/src/main/java/pl/allegro/braincode/api/config/RestApiConfig.java
@@ -1,5 +1,7 @@
 package pl.allegro.braincode.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,5 +27,12 @@ public class RestApiConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
